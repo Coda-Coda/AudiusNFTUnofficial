7c9a1d3de04e362d1c0acd79ebe91bc9d88256ad
Mason Elliott <mason.elliott12@gmail.com>
2021-05-29 15:35:44 +1200

    initial commit

---
+++ src/pages/Creator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Creator = () => {
+
+    return(
+        <div style={{color: "#194D33"}}>
+            <h1 >Creator</h1>;
+        </div>
+    )
+    
+}
+
+export default Creator;
---
+++ src/pages/Fans.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Fans = () => {
+
+    return(
+        <div style={{color: "#194D33"}}>
+            <h1 >Fans</h1>;
+        </div>
+    )
+
+
+    
+    
+    
+    
+
+}
+
+export default Fans;
